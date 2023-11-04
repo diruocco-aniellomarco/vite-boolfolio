14f667af889c160db717a652f8631659393eda36
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-11-04 10:04:42 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import ProjectList from "./components/projects/ProjectList.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: { ProjectList },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>Hello world</h1>
+    <ProjectList />
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Lista progetti",
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+</template>
+
+<style lang="scss"></style>
